ae3b4a7969e7e5e96ce870d73f36d2aad889f217
DKtruster <40022921+DKtruster@users.noreply.github.com>
2020-09-24 10:58:28 +0200

    GUI_SETUP

---
+++ main.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+from tkinter import *
+from tkinter.ttk import *
+
+# GUI Set-up made with help from: https://www.geeksforgeeks.org/python-gui-tkinter/
+
+w = tk.Tk()
+w.title("Game_name")
+w.geometry("1200x700")
+
+def updateCards():
+    for i in range(10):
+        stringHolder = "cardHolder" + str(i)
+        stringHolder = tk.Button(w, width=15, height=10,text=("Card "+str(i)))
+        stringHolder.config(bg='lightgreen')
+
+        if (i < 5):
+            stringHolder.place(relx=1, x=-50 - (i * 120), y=100, anchor=NE)
+        if (i >= 5):
+            stringHolder.place(relx=1, x=-50 - ((i - 5) * 120), y=350, anchor=NE)
+
+
+sceneUpdateButton = tk.Button(w, text='Update scene', width=15, height=10, command=updateCards)
+sceneUpdateButton.place(relx=1, x=-1000, y=150, anchor=NE)
+
+w.mainloop()
