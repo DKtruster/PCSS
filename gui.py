3eed8e0d304a13c600234b5cc93826b5cb3969f6
DKtruster <40022921+DKtruster@users.noreply.github.com>
2020-09-26 09:36:03 +0200

    origins_shop_classes

--- gui.py
+++ gui.py
@@ -7,7 +7,6 @@ class displayGUI():
     w.title("Game_name")
     w.geometry("1200x700")
 
-
     for i in range(10):
         stringHolder = "cardHolder" + str(i)
         stringHolder = tk.Button(w, width=15, height=10, text=("Card " + str(i)))
@@ -17,7 +16,6 @@ class displayGUI():
             stringHolder.place(relx=1, x=-50 - (i * 120), y=100, anchor=NE)
         if (i >= 5):
             stringHolder.place(relx=1, x=-50 - ((i - 5) * 120), y=350, anchor=NE)
-
 
     sceneUpdateButton = tk.Button(w, text='Update scene', width=15, height=10)
     sceneUpdateButton.place(relx=1, x=-1000, y=150, anchor=NE)
--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 from gui import displayGUI
+from cards import Cards
 
+# SETUP
+
+# DISPLAY CARDS
 b = displayGUI()
+
+# SHOP
