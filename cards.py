7f79ab0f161c0a514831378deaf55f23cdc6d3bc
DKtruster <40022921+DKtruster@users.noreply.github.com>
2020-09-26 12:17:02 +0200

    cardNumbers & dataTransfer between classes

--- cards.py
+++ cards.py
@@ -1,13 +1,18 @@
 class Cards:
+    __cardnumber = "0"
     __name = "NameHold"
     __origin = "OriginHold"
     __health = "10"
     __damage = "5"
 
-    def __init__(self, name: str, origin: str, health: int, damage: int):
+    def __init__(self, cardnumber: int , name: str, origin: str, health: int, damage: int):
+        self.__cardnumber = cardnumber
         self.__name = name
         self.__origin = origin
         self.__health = health
         self.__damage = damage
+
+    def get_name(self):
+        return self.__name
 
 
--- main.py
+++ main.py
@@ -10,8 +10,13 @@ class main:
     # Data loader from .txt files made with help from:
     # https://www.earthdatascience.org/courses/intro-to-earth-data-science/scientific-data-structures-python/numpy-arrays/import-txt-csv-files-numpy-arrays/
     dataLoader = np.genfromtxt('origins.txt', dtype='str')
+
     for lines in range(len(dataLoader)):
-        print(dataLoader[lines])
+        nameHolder = dataLoader[lines][0]
+        nameHolder = Cards(dataLoader[lines][0],dataLoader[lines][1],dataLoader[lines][2],dataLoader[lines][3],dataLoader[lines][4])
+        print(nameHolder.get_name())
+
+
 
     # DISPLAY CARDS
     b = displayGUI()
