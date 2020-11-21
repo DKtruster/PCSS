c52f1edcfff973ba80cc831928fe47934a1f743c
DKtruster <40022921+DKtruster@users.noreply.github.com>
2020-11-21 21:08:40 +0100

    cleared empty files

--- shop.py
+++
@@ -1,7 +0,0 @@
-# Load the players cards
-# Randomize 5 difference cards
-# Check if they already exist on the deck
-# Check if the player have enough currency
-# Load GUI, and give the player a new card
-
-# Different chances of appearing depending on their rarity/cost
